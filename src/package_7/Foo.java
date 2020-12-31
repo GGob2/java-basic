25dfd93605cbd22c48abc26c96e8587a23f74ab3
GGOB2 <rkdaudwh13@naver.com>
2021-01-01 02:41:04 +0900

    add : create class for showing example

---
+++ src/package_7/Foo.java
@@ -0,0 +1,10 @@
+package package_7;
+
+import java.util.ArrayList;
+import static java.lang.Math.random;
+import static java.lang.Math.PI;
+public class Foo {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+    }
+}
