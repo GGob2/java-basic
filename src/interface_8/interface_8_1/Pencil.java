e0e4d6b9bcd342fa0090bc1358942c9f531ae2c0
GGOB2 <rkdaudwh13@naver.com>
2021-01-09 00:55:40 +0900

    TIL_1 : interface, inheritance, abstract, override

--- src/interface_8/interface_8_1/Pencil.java
+++ src/interface_8/interface_8_1/Pencil.java
@@ -1,5 +1,11 @@
 package interface_8.interface_8_1;
 
 public interface Pencil {
-    public void shout();
+
+    public int PENCIL_LENGTH = 15;
+    public String PENCIL_BRAND = "JEDO";
+
+    public void writeEnglish(String englishSentence);
+    public void writeKorean(String hangulSentence);
+    public void broke();
 }
--- src/interface_8/interface_8_1/Student.java
+++ src/interface_8/interface_8_1/Student.java
@@ -4,9 +4,20 @@ public class Student implements Pencil{
     public static void main(String[] args) {
         System.out.println("this is student class");
     }
+    @Override
+    public void writeEnglish(String englishSentence) {
+        System.out.println("this is English sentence");
+    }
 
     @Override
-    public void shout() {
-        System.out.println("this is implemented by Pencil Interface");
+    public void writeKorean(String hangulSentence) {
+        System.out.println("this is Korean sentence");
+    }
+
+    @Override
+    public void broke() {
+        System.out.println("Ouch! it's broken");
     }
 }
+
+
