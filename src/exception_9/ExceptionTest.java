2aa369b7806a880df4c5cc48e2c9774b26954168
GGOB2 <rkdaudwh13@naver.com>
2021-01-16 19:53:38 +0900

    TIL: exception study on 9th week

---
+++ src/exception_9/ExceptionTest.java
@@ -0,0 +1,10 @@
+package exception_9;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 0;
+//        int z = x/y;
+//        System.out.println(z);
+    }
+}
