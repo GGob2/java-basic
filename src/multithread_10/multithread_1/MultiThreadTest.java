9a4cae30bcfb15e6274c0149df379b43de6bdbed
GGOB2 <rkdaudwh13@naver.com>
2021-01-23 02:22:08 +0900

    TIL: Thread Class Example

--- src/multithread_10/multithread_1/MultiThreadTest.java
+++ src/multithread_10/multithread_1/MultiThreadTest.java
@@ -1,7 +1,21 @@
 package multithread_10.multithread_1;
 
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            System.out.println("1");
+            Thread.sleep(1000);
+            System.out.println("2");
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class MultiThreadTest {
     public static void main(String[] args) {
-        System.out.println("10주차 시작!!! ");
+      Thread myThread = new MyThread();
+      myThread.run();
     }
 }
